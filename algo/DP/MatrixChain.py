93a01fe166987a77a9a291e02f93f3f8b7beea13
ZenSky123 <zensky@126.com>
2018-06-21 18:46:05 +0800

    添加非递归矩阵链乘法

--- algo/DP/MatrixChain.py
+++ algo/DP/MatrixChain.py
@@ -1,7 +1,8 @@
 INF = 99999
 
-m = [[0 for _ in range(100)] for _ in range(100)]
-s = [[0 for _ in range(100)] for _ in range(100)]
+n = 4
+m = [[0 for _ in range(n)] for _ in range(n)]
+s = [[0 for _ in range(n)] for _ in range(n)]
 
 
 def RecurMatrixChain(P, i, j):
@@ -19,8 +20,26 @@ def RecurMatrixChain(P, i, j):
     return m[i][j]
 
 
+def MatrixChain(P, n):
+    result = 0
+    for r in range(2, n + 1):
+        for i in range(1, n - r + 2):
+            j = i + r - 1
+
+            m[i][j] = m[i + 1][j] + P[i - 1] * P[i] * P[j]
+            s[i][j] = i
+            for k in range(i + 1, j):
+                t = m[i][k] + m[k + 1][j] + P[i - 1] * P[k] * P[j]
+                if t < m[i][j]:
+                    m[i][j] = t
+                    s[i][j] = k
+                    result = m[i][j]
+    return result
+
+
 if __name__ == '__main__':
     P = [
         10, 100, 5, 50
     ]
-    print(RecurMatrixChain(P, 1, 3))
+    # print(RecurMatrixChain(P, 1, n-1))
+    print(MatrixChain(P, 3))
